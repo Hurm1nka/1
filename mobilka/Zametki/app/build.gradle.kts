dd379e2f90626798e56fa99cfdec151ca5a2cf6a
Santrina <f.klokov00@gmail.com>
2026-06-04 14:03:56 +0300

    gotovo

--- mobilka/Zametki/app/build.gradle.kts
+++ mobilka/Zametki/app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -9,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.zametki"
-        minSdk = 34
+        minSdk = 33
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
@@ -30,18 +29,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
-    implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- mobilka/Zametki/app/src/main/java/com/example/zametki/Note.kt
+++
@@ -1,4 +0,0 @@
-package com.example.zametki
-
-
-data class Note(val title : String, val content : String)
